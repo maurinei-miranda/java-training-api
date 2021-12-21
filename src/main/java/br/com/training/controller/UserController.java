1d4ac3b950513cd2e1fd9c8fcd1b471300d61fc5
maurinei-miranda <maurinei.develop@gmail.com>
2021-12-21 12:32:51 -0300

    add put and delete
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/main/java/br/com/training/controller/UserController.java
+++ src/main/java/br/com/training/controller/UserController.java
@@ -3,8 +3,10 @@ package br.com.training.controller;
 import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.*;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import br.com.training.model.User;
 import br.com.training.repository.UserRepository;
@@ -28,5 +30,24 @@ public class UserController {
     public User getUser (@PathVariable String cpf){
         return userRepository.findByCpf(cpf);
     }
-
+	
+	@PutMapping (value = "/user/{cpf}")
+	@ResponseStatus(HttpStatus.OK)
+	public User updateUser(@RequestBody @Valid User userUpdate, @PathVariable String cpf) {
+		User usuario = userRepository.findByCpf(cpf);
+		usuario.setName(userUpdate.getName());
+		usuario.setEmail(userUpdate.getEmail());
+		usuario.setBirthDate(userUpdate.getBirthDate());
+		usuario.setCpf(userUpdate.getCpf());
+		
+		return userRepository.save(usuario);
+	}
+	
+	@DeleteMapping (value = "/user/{cpf}")
+	public void delUser(@PathVariable String cpf) {
+		User usuario = userRepository.findByCpf(cpf);
+		userRepository.deleteById(usuario.getId());
+		
+	}
+	
 }
