50687e14b1517317aeda6928b3bfe0ea2fa7eeba
maurinei-miranda <maurinei.develop@gmail.com>
2021-12-22 15:08:05 -0300

    add service layer
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/main/java/br/com/training/controller/UserController.java
+++ src/main/java/br/com/training/controller/UserController.java
@@ -2,52 +2,40 @@ package br.com.training.controller;
 
 import javax.validation.Valid;
 
+import br.com.training.service.UserService;
 import org.springframework.web.bind.annotation.*;
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import br.com.training.model.User;
-import br.com.training.repository.UserRepository;
 
 @RestController
 @RestControllerAdvice
 @RequestMapping("/users")
 public class UserController {
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public User createUser(@RequestBody @Valid User user) {
-		return userRepository.save(user);
-	}
-
-	@GetMapping (value = "/{cpf}")
-	@ResponseStatus(HttpStatus.OK)
-    public User getUser (@PathVariable String cpf){
-        return userRepository.findByCpf(cpf);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody @Valid User user) {
+        return userService.createUser(user);
     }
-	
-	@PutMapping (value = "/user/{cpf}")
-	@ResponseStatus(HttpStatus.OK)
-	public User updateUser(@RequestBody @Valid User userUpdate, @PathVariable String cpf) {
-		User usuario = userRepository.findByCpf(cpf);
-		usuario.setName(userUpdate.getName());
-		usuario.setEmail(userUpdate.getEmail());
-		usuario.setBirthDate(userUpdate.getBirthDate());
-		usuario.setCpf(userUpdate.getCpf());
-		
-		return userRepository.save(usuario);
-	}
-	
-	@DeleteMapping (value = "/user/{cpf}")
-	public void delUser(@PathVariable String cpf) {
-		User usuario = userRepository.findByCpf(cpf);
-		userRepository.deleteById(usuario.getId());
-		
-	}
-	
+
+    @GetMapping(value = "/{cpf}")
+    @ResponseStatus(HttpStatus.OK)
+    public User getUser(@PathVariable String cpf) {
+        return userService.findByCpf(cpf);
+    }
+
+    @PutMapping(value = "/{cpf}")
+    public User updateUser(@PathVariable String cpf, @RequestBody User newUser) {
+        return userService.updateUser(cpf, newUser);
+    }
+
+    @DeleteMapping(value = "/{cpf}")
+    public User deleteUser(@PathVariable String cpf) {
+        return userService.deleteUser(cpf);
+    }
 }
---
+++ src/main/java/br/com/training/service/UserService.java
@@ -0,0 +1,53 @@
+package br.com.training.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
+import br.com.training.model.User;
+import br.com.training.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(@RequestBody @Valid User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByCpf(String cpf) {
+        try {
+            User user = userRepository.findByCpf(cpf);
+        } catch (NullPointerException e) {
+            System.out.println("User not found" + e);
+        }
+        return userRepository.findByCpf(cpf);
+    }
+
+    public User updateUser(String cpf, User newUser) {
+        try {
+            User user = userRepository.findByCpf(cpf);
+        } catch (
+                NullPointerException e) {
+            System.out.println("User not found" + e);
+        }
+
+        User user = userRepository.findByCpf(cpf);
+        user.setName(newUser.getName());
+        user.setEmail(newUser.getEmail());
+        user.setBirthDate(newUser.getBirthDate());
+        user.setCpf(newUser.getCpf());
+
+        return userRepository.save(user);
+    }
+
+    public User deleteUser(String cpf) {
+        User user = userRepository.findByCpf(cpf);
+        userRepository.delete(user);
+        return user;
+    }
+}
