2c42d3e5eaee871ee64cf58a9f5f3a04a241daf7
maurinei-miranda <maurinei.develop@gmail.com>
2022-01-10 05:25:24 -0300

    add annotations on UserForm
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/main/java/br/com/training/controller/dto/UserForm.java
+++ src/main/java/br/com/training/controller/dto/UserForm.java
@@ -1,15 +1,24 @@
 package br.com.training.controller.dto;
 
 import br.com.training.model.User;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 
 public class UserForm {
 
+    @NotBlank
     private String name;
+    @CPF
     private String cpf;
+    @Email
     private String email;
+    @NotNull
     private LocalDate birthDate;
 
     public UserForm(String name, String cpf, String email, LocalDate birthDate) {
