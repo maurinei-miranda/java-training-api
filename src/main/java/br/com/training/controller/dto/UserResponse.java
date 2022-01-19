7e8daf541678329455b0b28f94a4991e0189ea22
maurinei-miranda <maurinei.develop@gmail.com>
2022-01-19 19:01:42 -0300

    lint files
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/main/java/br/com/training/controller/dto/UserResponse.java
+++ src/main/java/br/com/training/controller/dto/UserResponse.java
@@ -3,7 +3,6 @@ package br.com.training.controller.dto;
 import br.com.training.model.User;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 public class UserResponse {
     private String name;
@@ -15,26 +14,26 @@ public class UserResponse {
         return new UserResponse(user.getName(), user.getEmail(), user.getCpf(), user.getBirthDate());
     }
 
-    public UserResponse(String name, String email, String cpf, LocalDate birthDate){
+    public UserResponse(String name, String email, String cpf, LocalDate birthDate) {
         this.name = name;
         this.email = email;
         this.cpf = cpf;
         this.birthDate = birthDate;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
-    public String getCpf(){
+    public String getCpf() {
         return cpf;
     }
 
-    public LocalDate getBirthDate(){
+    public LocalDate getBirthDate() {
         return birthDate;
     }
 
