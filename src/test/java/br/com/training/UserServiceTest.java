ed2aa0d6902ca5c4c505cee78ffae407ae171b09
maurinei-miranda <maurinei.develop@gmail.com>
2022-01-26 18:48:37 -0300

    Add test to User service layer
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/test/java/br/com/training/UserServiceTest.java
+++ src/test/java/br/com/training/UserServiceTest.java
@@ -1,6 +1,5 @@
 package br.com.training;
 
-import br.com.training.exceptions.ApiError;
 import br.com.training.model.User;
 import br.com.training.repository.UserRepository;
 import br.com.training.service.UserService;
@@ -9,14 +8,14 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.LocalDate;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -38,14 +37,12 @@ public class UserServiceTest {
         verify(userRepository, times(1)).findByCpf(user.getCpf());
     }
 
+    User defaultUser = new User("Robert Greene", "user@gmail.com", "23327434050", LocalDate.parse("1994-03-31"));
+    User updatedData = new User("Geralt The Rivia", "geralt@gmail.com", "86349822030", LocalDate.parse("1168-05-05"));
+
     @Test
     public void testFindUserByCpfSuccess() {
-        User user = new User();
-        user.setId(1L);
-        user.setName("John Green");
-        user.setCpf("60711829055");
-        user.setEmail("user@gmail.com");
-        user.setBirthDate(LocalDate.parse("1994-03-31"));
+        User user = defaultUser;
         when(userRepository.findByCpf(user.getCpf())).thenReturn(Optional.of(user));
         userService.findByCpf(user.getCpf());
         verify(userRepository, times(1)).findByCpf(user.getCpf());
@@ -54,14 +51,47 @@ public class UserServiceTest {
 
     @Test
     public void testSaveSuccess() {
-        User user = new User();
-        user.setName("John Green");
-        user.setCpf("23327434050");
-        user.setEmail("user@gmail.com");
-        LocalDate birthDate = LocalDate.parse("1994-03-31");
-        user.setBirthDate(birthDate);
+        User user = defaultUser;
         userService.save(user);
         verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    public void testDeleteUserSuccess() {
+        User user = defaultUser;
+        when(userRepository.findByCpf(user.getCpf())).thenReturn(Optional.of(user));
+        userService.delete(user);
+        verify(userRepository, times(1)).delete(user);
+    }
+
+    @Test
+    public void testDeleteUserNotFound() {
+        User user = defaultUser;
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            userService.delete(user);
+        });
+        verify(userRepository, times(1)).findByCpf(user.getCpf());
+        assertEquals("cpf not found: " + user.getCpf(), exception.getMessage());
+    }
+
+    @Test
+    public void testUpdateUserSuccess() {
+        User user = defaultUser;
+        User updated = updatedData;
+        when(userRepository.findByCpf(user.getCpf())).thenReturn(Optional.of(user));
+        userService.update(user.getCpf(), updated);
+        verify(userRepository, times(1)).save(updated);
+    }
+
+    @Test
+    public void testUpdateUserNotFound() {
+        User user = defaultUser;
+        User updated = updatedData;
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> {
+            userService.update(user.getCpf(), updated);
+        });
+        verify(userRepository, times(1)).findByCpf(user.getCpf());
+        assertEquals("cpf not found: " + user.getCpf(), exception.getMessage());
     }
 
 
