066c8c88b51d9029c12426100a88641df7c40a3f
maurinei-miranda <maurinei.develop@gmail.com>
2022-02-16 22:07:47 -0300

    enable feign client
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/main/java/br/com/training/TrainingApiApplication.java
+++ src/main/java/br/com/training/TrainingApiApplication.java
@@ -2,12 +2,14 @@ package br.com.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TrainingApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainingApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingApiApplication.class, args);
+    }
 
 }
