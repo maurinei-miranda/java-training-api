450c6a2674f71b6794f9f94ba488d7378bd94086
maurinei-miranda <maurinei.develop@gmail.com>
2022-02-16 22:13:33 -0300

    add diseaseClient
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

---
+++ src/main/java/br/com/training/interfaces/DiseasesClient.java
@@ -0,0 +1,13 @@
+package br.com.training.interfaces;
+
+import br.com.training.models.Disease;
+import br.com.training.models.Root;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "diseases", url = "https://disease-info-api.herokuapp.com")
+public interface DiseasesClient {
+    @GetMapping("/diseases")
+    Root findAll();
+    
+}
