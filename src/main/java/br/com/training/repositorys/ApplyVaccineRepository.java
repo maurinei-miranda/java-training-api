46d5e68b7fcf4c7e51eff69f843d6fd3b333fc44
maurinei-miranda <maurinei.develop@gmail.com>
2022-03-07 04:06:53 -0300

    update repository layer
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/main/java/br/com/training/repositorys/ApplyVaccineRepository.java
+++ src/main/java/br/com/training/repositorys/ApplyVaccineRepository.java
@@ -5,7 +5,9 @@ import br.com.training.models.ApplyVaccine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface ApplyVaccineRepository extends JpaRepository<ApplyVaccine, Long> {
     ArrayList<ApplyVaccine> findAll();
+    List<ApplyVaccine> findAllByUserId(Long id);
 }
