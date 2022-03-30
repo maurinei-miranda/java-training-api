1115933aa42e446be9715c81a11d8f1fab9429a0
maurinei-miranda <maurinei.develop@gmail.com>
2022-03-30 03:08:34 -0300

    VaccineService Tests - 1/2
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

---
+++ src/test/java/br/com/training/VaccineServiceTest.java
@@ -0,0 +1,86 @@
+package br.com.training;
+
+import br.com.training.models.Disease;
+import br.com.training.models.Vaccine;
+import br.com.training.repositorys.VaccineRepository;
+import br.com.training.services.DiseaseService;
+import br.com.training.services.VaccineService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VaccineServiceTest {
+
+    @Mock
+    private VaccineRepository vaccineRepository;
+
+    @Mock
+    private DiseaseService diseaseService;
+
+    @InjectMocks
+    private VaccineService vaccineService;
+
+    @Test
+    public void testFindVaccineByNameSuccess_Return200() {
+        LocalDate localDate = LocalDate.parse("2022-03-28");
+        Vaccine vaccine = new Vaccine("Pfizer",
+                "Covid-19",
+                "Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.",
+                5,
+                4,
+                localDate,
+                localDate);
+        when(vaccineRepository.findByName(vaccine.getName())).thenReturn(Optional.of(vaccine));
+        vaccineService.findByName("Pfizer");
+        verify(vaccineRepository, times(1)).findByName(vaccine.getName());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testFindVaccineByName_NoSuchElement() {
+        vaccineService.findByName("Pfizer");
+        verify(vaccineRepository, times(1)).findByName("Pfizer");
+    }
+
+    @Test
+    public void testSaveVaccine_Return201() {
+        Vaccine vaccine = new Vaccine();
+        Disease disease = new Disease();
+        ArrayList<String> facts = new ArrayList<>();
+
+        disease.setName("Covid-19");
+        disease.setFacts(facts);
+        facts.add("Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.");
+
+        vaccine.setDiseaseName("Covid-19");
+
+        when(diseaseService.findByName("Covid-19")).thenReturn(Optional.of(disease));
+        vaccineService.save(vaccine);
+        verify(vaccineRepository, times(1)).save(vaccine);
+
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testSaveVaccine_DiseaseNotFound() {
+        Vaccine vaccine = new Vaccine();
+        Disease disease = new Disease();
+        ArrayList<String> facts = new ArrayList<>();
+
+        disease.setName("Covid-19");
+        disease.setFacts(facts);
+        facts.add("Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.");
+
+        vaccine.setDiseaseName("Covid-19");
+
+        vaccineService.save(vaccine);
+    }
+}
