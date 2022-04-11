b22e1a4327b6677cbef379d4a404fe1ef7af67f3
maurinei-miranda <maurinei.develop@gmail.com>
2022-04-11 20:39:05 -0300

    buildApplyVaccineTests_Fail
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/test/java/br/com/training/ApplyVaccineServiceTest.java
+++ src/test/java/br/com/training/ApplyVaccineServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.training;
 
 import br.com.training.dto.ApplyVaccineForm;
+import br.com.training.exceptions.VaccineRestrictions;
 import br.com.training.models.ApplyVaccine;
 import br.com.training.models.User;
 import br.com.training.models.Vaccine;
@@ -15,8 +16,11 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -125,5 +129,66 @@ public class ApplyVaccineServiceTest {
         ApplyVaccine resp = applyVaccineService.buildApplyVaccine(applyVaccineForm);
         assertEquals(applyVaccine.getVaccine(), resp.getVaccine());
         assertEquals(applyVaccine.getUser(), resp.getUser());
+    }
+
+    @Test
+    public void buildApplyVaccine_ReturnExceptionThirtyDays() {
+        LocalDate localDate = LocalDate.now();
+        User user = new User("Robert Greene", "user@gmail.com", "23327434050", localDate);
+        user.setId(1L);
+        Vaccine vaccine = new Vaccine("Pfizer",
+                "Covid-19",
+                "Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.",
+                5,
+                4,
+                localDate,
+                localDate);
+
+        ApplyVaccine oldApplyVaccine = new ApplyVaccine();
+        oldApplyVaccine.setVaccine(vaccine);
+        oldApplyVaccine.setUser(user);
+        oldApplyVaccine.setDate(localDate);
+
+        List<ApplyVaccine> responseMock = new ArrayList<>();
+        responseMock.add(oldApplyVaccine);
+
+        when(userService.findByCpf(user.getCpf())).thenReturn(user);
+        when(vaccineService.findByName(vaccine.getName())).thenReturn(vaccine);
+        when(applyVaccineService.findByUserCpf(user.getCpf())).thenReturn(responseMock);
+
+        ApplyVaccineForm applyVaccineForm = new ApplyVaccineForm(user.getCpf(), vaccine.getName(), localDate);
+        VaccineRestrictions ex = assertThrows(VaccineRestrictions.class, () -> applyVaccineService.buildApplyVaccine(applyVaccineForm));
+        assertEquals("Usuário já recebeu uma dose dessa vacina nos ultimos 30 dias.", ex.getMessage());
+    }
+
+    @Test
+    public void buildApplyVaccineTest_ReturnExceptionMaxDoses() {
+        LocalDate localDate = LocalDate.parse("2022-04-11");
+        User user = new User("Robert Greene", "user@gmail.com", "23327434050", localDate);
+        user.setId(1L);
+        Vaccine vaccine = new Vaccine("Pfizer",
+                "Covid-19",
+                "Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.",
+                5,
+                2,
+                localDate,
+                localDate);
+
+        ApplyVaccine oldApplyVaccine = new ApplyVaccine();
+        oldApplyVaccine.setVaccine(vaccine);
+        oldApplyVaccine.setUser(user);
+        oldApplyVaccine.setDate(localDate);
+
+        List<ApplyVaccine> responseMock = new ArrayList<>();
+        responseMock.add(oldApplyVaccine);
+        responseMock.add(oldApplyVaccine);
+
+        when(userService.findByCpf(user.getCpf())).thenReturn(user);
+        when(vaccineService.findByName(vaccine.getName())).thenReturn(vaccine);
+        when(applyVaccineService.findByUserCpf(user.getCpf())).thenReturn(responseMock);
+
+        ApplyVaccineForm applyVaccineForm = new ApplyVaccineForm(user.getCpf(), vaccine.getName(), localDate);
+        VaccineRestrictions ex = assertThrows(VaccineRestrictions.class, () -> applyVaccineService.buildApplyVaccine(applyVaccineForm));
+        assertEquals("The user Robert Greene has reached the maximum number of vaccine doses.", ex.getMessage());
     }
 }
