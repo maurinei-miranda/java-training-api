79d73f1932466171888b7f6361106cc07c03ee62
maurinei-miranda <maurinei.develop@gmail.com>
2022-04-21 17:07:07 -0300

    lint code, update ApiResponses
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- src/main/java/br/com/training/controllers/UserController.java
+++ src/main/java/br/com/training/controllers/UserController.java
@@ -19,45 +19,39 @@ import java.util.List;
 @RestControllerAdvice
 @RequestMapping("/users")
 public class UserController {
-
     @Autowired
     private UserService userService;
 
     @Autowired
     private MapStructMapper mapStructMapper;
 
-    @ApiResponses(
-            value = {
-                    @ApiResponse(code = 200, message = "Return a list of users")
-            }
-    )
-    @GetMapping(value = "/all", produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return a list of users"),
+            @ApiResponse(code = 500, message = "Some unmapped exception occurred.")
+    })
+    @GetMapping(produces = "application/json")
     public ResponseEntity<List<UserResponse>> findAllUsers() {
         List<User> userList = userService.findAllUsers();
         List<UserResponse> userResponseList = mapStructMapper.userListToUserResponse(userList);
         return new ResponseEntity<>(userResponseList, HttpStatus.OK);
     }
 
-    @ApiResponses(value =
-            {
-                    @ApiResponse(code = 200, message = "Return a user."),
-                    @ApiResponse(code = 404, message = "User not found."),
-                    @ApiResponse(code = 500, message = "An exception has occurred.")
-            }
-    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return a user."),
+            @ApiResponse(code = 404, message = "User not found."),
+            @ApiResponse(code = 500, message = "Some unmapped exception occurred.")
+    })
     @GetMapping(value = "/{cpf}", produces = "application/json")
     public ResponseEntity<UserResponse> findByCpf(@PathVariable String cpf) {
         User user = userService.findByCpf(cpf);
         return new ResponseEntity<>(mapStructMapper.userToUserResponse(user), HttpStatus.OK);
     }
 
-    @ApiResponses(value =
-            {
-                    @ApiResponse(code = 201, message = "User created successfully. Return created user data."),
-                    @ApiResponse(code = 400, message = "Invalid request. Return details of occurred exception."),
-                    @ApiResponse(code = 500, message = "Some exception not mapped occurred.")
-            }
-    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "User created successfully. Return created user data."),
+            @ApiResponse(code = 400, message = "Invalid request. Return details of the occurred exception."),
+            @ApiResponse(code = 500, message = "Some unmapped exception occurred.")
+    })
     @PostMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserForm userForm) {
         User user = mapStructMapper.userDtoToUser(userForm);
@@ -65,13 +59,11 @@ public class UserController {
         return new ResponseEntity<>(mapStructMapper.userToUserResponse(user), HttpStatus.CREATED);
     }
 
-    @ApiResponses(value =
-            {
-                    @ApiResponse(code = 200, message = "User updated successfully. Return updated user data."),
-                    @ApiResponse(code = 400, message = "Invalid request. Return details of occurred exception."),
-                    @ApiResponse(code = 500, message = "Some exception not mapped occurred.")
-            }
-    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "User updated successfully. Return updated user data."),
+            @ApiResponse(code = 400, message = "Invalid request. Return details of occurred exception."),
+            @ApiResponse(code = 500, message = "Some unmapped exception occurred.")
+    })
     @PutMapping(value = "/{cpf}", produces = "application/json", consumes = "application/json")
     public ResponseEntity<UserResponse> updateUser(@PathVariable String cpf, @RequestBody @Valid UserForm userForm) {
         User user = mapStructMapper.userDtoToUser(userForm);
@@ -79,13 +71,11 @@ public class UserController {
         return new ResponseEntity<>(mapStructMapper.userToUserResponse(user), HttpStatus.OK);
     }
 
-    @ApiResponses(value =
-            {
-                    @ApiResponse(code = 200, message = "User deleted successfully. Return deleted user data."),
-                    @ApiResponse(code = 404, message = "User not found."),
-                    @ApiResponse(code = 500, message = "Some exception not mapped occurred.")
-            }
-    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "User deleted successfully. Return deleted user data."),
+            @ApiResponse(code = 404, message = "User not found."),
+            @ApiResponse(code = 500, message = "Some unmapped exception occurred.")
+    })
     @DeleteMapping(value = "/{cpf}", produces = "application/json")
     public ResponseEntity<UserResponse> deleteUser(@PathVariable String cpf) {
         User user = userService.findByCpf(cpf);
